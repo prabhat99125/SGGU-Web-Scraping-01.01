34bc82ecba9e04c485587a07f93489c3b673998f
prabaht99125 <iturathava@gmail.com>
2024-12-27 23:13:17 +0530

    upDate File

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const puppeteer = require("puppeteer");
+const collegeName = require("./collegeName");
 // const ReadHtml = require("./htmlRead");
-
-let setNo = 2441;
-let spdId = 2022002955;
-let students = 200;
-const AcademicYear = '2022-2023';
-const degree = "B.Com. Semester - 1 (December - 2022)";
+console.log(collegeName(50));
+let setNo = 1841;
+let spdId = 2022002124;
+let students = 50000;
+const AcademicYear = '2023-2024';
+const degree = "B.Com. Semester - 4 (April - 2024)";
 
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Path to your Chrome
+        defaultViewport: null,
         slowMo: true,
         args: ["--start-maximized"]
     });
-    try {
-
-    } catch (error) {
-        console.log(error)
-    }
     try {
         const [page] = await browser.pages();
         page.setDefaultTimeout(60000);
@@ -58,6 +56,7 @@ const degree = "B.Com. Semester - 1 (December - 2022)";
         await page.type("#rtxtSPDID_text", String(spdId));
         await page.click("#ibtnSubmit");
 
+        let tryCount = 0;
         // Perform Submission for Multiple Entries
         const performSubmission = async (seatNo, spdIdLocal) => {
             await page.waitForSelector("#lblStudentName", { visible: true });
@@ -74,10 +73,40 @@ const degree = "B.Com. Semester - 1 (December - 2022)";
 
             await page.click("#ibtnSubmit");
             await page.waitForSelector("#lblSPDIDValue", { visible: true });
+            // if (true) {
+            //     seatNo = seatNo - 1;
+            //     tryCount++;
+            //     if (tryCount >= 10) {
+            //         console.log(tryCount);
+            //         seatNo++
+            //         spdId = spdId - 10;
+            //         tryCount = 0
+            //     }
+            // } else {
+            //     // const html = await page.content();
+            //     // setNo = await ReadHtml(html, Number(seatNo));
+            //     // console.log("Processed Seat No:", setNo);
+            // }
+            const textToCheck = "Student SPDID do not Match";
+            const doesTextExist = await page.evaluate((text) => {
+                return document.body.textContent.includes(text);
+            }, textToCheck);
 
-            // const html = await page.content();
-            // setNo = await ReadHtml(html, Number(seatNo));
-            // console.log("Processed Seat No:", setNo);
+            if (doesTextExist) {
+                setNo--
+                tryCount++;
+                console.log("error:", tryCount, seatNo)
+                if (tryCount >= 10) {
+                    console.log(tryCount, setNo, spdId);
+                    setNo++
+                    spdId = spdId - 10;
+                    tryCount = 0
+                }
+            } else {
+                tryCount = 0;
+                console.log("done", seatNo, spdIdLocal)
+            }
+
         };
 
         const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -86,6 +115,7 @@ const degree = "B.Com. Semester - 1 (December - 2022)";
             await performSubmission(String(setNo), String(spdId));
             setNo++;
             spdId++;
+            // console.log("upDate : ", setNo, spdId);
             await delay(500); // Wait 0.5 second between submissions
         }
 
---
+++ collegeName.js
@@ -0,0 +1,68 @@
+const collegeName = (setNO) => {
+    if (setNO >= 1972 && setNO <= 1995) {
+        return "Adivasi Commerce College, Santrampur"
+    }
+    else if (setNO >= 2121 && setNO <= 2212) {
+        return "Dr. Thakorbhai Patel Girls CommerceCollege, Akota"
+
+    } else if (setNO >= 1344 && setNO <= 1455) {
+        return "D.P. Pandya Commerce College,Lunawada"
+
+    } else if (setNO >= 2229 && setNO <= 2248) {
+        return "Eklavya Commerce College, Kalarani"
+
+    } else if (setNO >= 1663 && setNO <= 1744) {
+        return "Government Commerce College,Naswadi"
+
+    } else if (setNO >= 812 && setNO <= 843) {
+        return "K.S.S. Commerce College, DevgadhBaria"
+
+    } else if (setNO >= 1 && setNO <= 100) {
+        return "L.K.L. Doshi Commerce College,Balasinor"
+
+    } else if (setNO >= 849 && setNO <= 1063) {
+        return "M & V Commerce College, Halol"
+
+    } else if (setNO >= 1163 && setNO <= 1336) {
+        return "M.M. Gandhi Commerce College, Kalol"
+
+    } else if (setNO >= 2257 && setNO <= 2436) {
+        return "Navjivan Commerce College, Dahod"
+
+    } else if (setNO >= 1121 && setNO <= 1160) {
+        return "S.H. Gardi Commerce College,Kakanpur"
+
+    }
+    else if (setNO >= 113 && setNO <= 342) {
+        return "Sheth T.C. Kapadia Commerce College,Bodeli"
+    }
+    else if (setNO >= 1099 && setNO <= 1117) {
+        return "Shri K.R. Desai Commerce College,Jhalod"
+    }
+    else if (setNO >= 360 && setNO <= 461) {
+        return "Shri S.G. Patel Commerce College,Chhotaudepur"
+
+    } else if (setNO >= 1557 && setNO <= 1649) {
+        return "Shri. M.C.Patel Commerce College,Muval"
+
+    } else if (setNO >= 2469 && setNO <= 2635) {
+        return "Shri. Sarvajanik Commerce College,Godhra"
+
+    } else if (setNO >= 1466 && setNO <= 1555) {
+        return "Smt. H.C.Patel (Kandari) CommerceCollege, Miyagam Karjan"
+
+    } else if (setNO >= 2002 && setNO <= 2090) {
+        return "Smt. L.M. Patel Commerce College,Savli"
+
+    } else if (setNO >= 1754 && setNO <= 1849) {
+        return "Smt. N.C. Zaveri Commerce College,Pipaliya"
+
+    } else if (setNO >= 485 && setNO <= 794) {
+        return "Smt. S.C.P.F. Commerce College,Dabhoi"
+
+    } else if (setNO >= 1858 && setNO <= 1965) {
+        return "The SNSB Commerce College,Sankheda"
+
+    }
+}
+module.exports = collegeName;
